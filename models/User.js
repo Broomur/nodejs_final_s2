a7c04218d2ca428715bb8b58ab8248caf9f83d28
Simon Chauvel <simonchauvel@gmail.com>
2023-11-24 10:18:52 +0100

    base_setup

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "./index.js";
+
+const UserModel = mongoose.model("UserModel", mongoose.Schema({
+	firstname: {
+		type: String,
+		required: true
+	},
+	lastname: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+}))
+
+export default UserModel;
